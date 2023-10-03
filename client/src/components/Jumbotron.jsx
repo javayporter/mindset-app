17679f9d949d1d3c3e2794ca8546a4f1a5954269
“javayporter” <javayscript@gmail.com>
2023-10-03 19:17:29 -0400

    make hero responsive, center divs on mobile view

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "../stylesheets/Jumbotron.css";
 import mindImage from "../images/purple_mind_big.jpeg";
 import "../stylesheets/Jumbotron.css";
 
 //Using this as Hero Section will rename files
 export const Jumbotron = () => {
-  const heading1 = "Welcome to";
+  const heading1 = "Welcome to the";
   const description =
     "The mindset app was designed to help you get through your day. Take care of your mind";
-  const heading2 = "the Mindset App";
+  const heading2 = "Mindset App";
   return (
     <div className="hero">
       <div className="left">
@@ -17,9 +18,13 @@ export const Jumbotron = () => {
           <br />
           <span>{heading2}</span>
         </h1>
-        <p>{description}</p>
-        <button>Learn More</button>
-        <button>Subscribe</button>
+        <p class="app-description">{description}</p>
+        <div className="buttons">
+          <Button variant="light">Learn More</Button>
+          <Button className="subscribe-btn" variant="success">
+            Subscribe
+          </Button>
+        </div>
       </div>
       <div className="right">
         <img src={mindImage} alt="mind flowers" />
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,22 +3,11 @@ import { TextArea } from "../components/TextArea";
 import { Jumbotron } from "../components/Jumbotron";
 
 export const Home = () => {
-  const heading = "Welcome to the Mindset App";
-  const description =
-    "The mindset app was designed to help you get through your day.";
-  const text = "Take care of your mind";
-
-  const textContent = {
-    heading: heading,
-    description: description,
-    text: text,
-  };
   return (
     <div>
       <div className="">
         <Jumbotron />
       </div>
-      <TextArea text={textContent} />
     </div>
   );
 };
