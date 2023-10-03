0b862ed1d42187bc4769d535c31c53ca86cacd0e
“javayporter” <javayscript@gmail.com>
2023-10-03 19:34:55 -0400

    more updates to page

--- client/src/components/Jumbotron.jsx
+++ client/src/components/Jumbotron.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import "../stylesheets/Jumbotron.css";
+
 import mindImage from "../images/purple_mind_big.jpeg";
+
+import "../stylesheets/Jumbotron.css";
 import "../stylesheets/Jumbotron.css";
 
 //Using this as Hero Section will rename files
 export const Jumbotron = () => {
   const heading1 = "Welcome to the";
-  const description =
-    "The mindset app was designed to help you get through your day. Take care of your mind";
   const heading2 = "Mindset App";
+  const description =
+    "The mindset app was designed to help you get through your day. Take care of your mind.";
+
   return (
     <div className="hero">
       <div className="left">
         <h1>
           <span>{heading1}</span>
           <br />
-          <span>{heading2}</span>
+          <span class="special-heading">{heading2}</span>
         </h1>
         <p class="app-description">{description}</p>
         <div className="buttons">
@@ -30,8 +33,5 @@ export const Jumbotron = () => {
         <img src={mindImage} alt="mind flowers" />
       </div>
     </div>
-    // <div className="jumbotron-cus" id="jumbo-comp">
-    //   <img src={mindImage} width="100px" height="400px" alt="mind flowers" />
-    // </div>
   );
 };
